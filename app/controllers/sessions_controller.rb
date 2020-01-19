3009c48a13f7712c2fa209e24f2b840753e82c5d
suomi-libereso <f5631g@gmail.com>
2020-01-19 04:54:59 +0000

    #13 remember-me

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,14 @@ class SessionsController < ApplicationController
   end
   
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
-    if @user && @user.authenticate(params[:session][:password])
+    #@取った方が良い？
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
-      log_in @user
-      redirect_to organization_user_path(@user.organization,@user)
+      redirect_to organization_user_path(user.organization,user)
     else
       flash.now[:danger] = 'メールアドレスまたはパスワードが間違っています'
       render 'new'
--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -37,4 +37,42 @@ describe "user_login", type: :feature do
       expect(page).to have_selector '.alert', text: "メールアドレスまたはパスワードが間違っています"
     end
   end
+  
+  xit "login with remembering" do 
+    visit '/login'
+      fill_in "user_email_login", with: @user.email
+      fill_in "user_password_login", with: @user.password
+      
+      #remember_me をチェック
+      check '次回から自動的にログイン'
+      
+      
+      click_button "ログイン"
+      
+      expect(page).to have_selector '.current_user_id', text: "#{@user.id}"
+      expect(page).to have_selector '.current_user_name', text: "#{@user.name}"
+      
+      #cookieがnilでないことを確認する→いったんやめる
+      #expect(response.cookies.signed[:user_id]).to eq @user.id
+      #expect(response.cookies['remember_token']).to_not eq nil
+      #remember_digestがnilでないことを確認する
+      expect(@user.remember_digest).not_to be nil
+  end
+  
+  it "login without remembering" do
+    visit '/login'
+      fill_in "user_email_login", with: @user.email
+      fill_in "user_password_login", with: @user.password
+      
+      #remember_me をチェックアウト
+      uncheck '次回から自動的にログイン'
+      
+      click_button "ログイン"
+      
+      
+      expect(@user.remember_digest).to be nil
+      
+      #cookieがnilであることを確認する
+      #expect(response.cookies['sample']).to eq 'ok'
+  end
 end
